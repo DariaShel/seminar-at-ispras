deffbe0253d6e9c655d19a1438975fd33c915b1e
Konstantin Arkhipenko <arkhipenko@ispras.ru>
2021-02-04 20:16:03 +0300

    Add base model and ResNet skeleton

--- adversarial_sandbox/models/base_tf_model.py
+++ adversarial_sandbox/models/base_tf_model.py
@@ -5,8 +5,32 @@ import tensorflow as tf
 
 class BaseTFModel(ABC):
     @abstractmethod
-    def get_keras_model() -> tf.keras.Model:
+    def __init__(self) -> None:
+        """Create a Keras classification model to be attacked.
+
+        The model must have an input shape of (224, 224, 3) and uint8 (0..255)
+        dtype. Any preprocessing (convert to floats, normalize) should also be
+        a part of this model.
+        In subclasses, assign the model to some protected attribute (with
+        underscore) and make it accessible via keras_model() property.
+        """
         pass
 
-    def compute_grad_input(x):
+    @property
+    @abstractmethod
+    def keras_model(self) -> tf.keras.Model:
+        pass
+
+    def compute_grad_input(x: tf.Tensor, y_true: tf.Tensor) -> tf.Tensor:
+        """Compute gradient of loss with respect to inputs.
+
+        Hint: use GradientTape and sparse_categorical_crossentropy().
+
+        Args:
+            x: a batch of input images, shape (batch_size, 224, 224, 3)
+            y: ground truth class indices, shape (batch_size,)
+
+        Returns:
+            Gradients tensor, shape (batch_size, 224, 224, 3)
+        """
         raise NotImplementedError
--- adversarial_sandbox/models/resnet.py
+++ adversarial_sandbox/models/resnet.py
@@ -2,4 +2,20 @@ from .base_tf_model import BaseTFModel
 
 
 class ResNet50Model(BaseTFModel):
-    pass
+    """A wrapper class for ResNet50 classification model.
+
+    Use tf.keras.applications to obtain the core network.
+    """
+
+    def __init__(self) -> None:
+        """Create ResNet50 classification model.
+
+        tf.keras.applications provides a preprocessing method for its ResNet50
+        model, please use it here.
+        """
+        super().__init__()
+        # TODO (DariaShel): implement model
+
+    @property
+    def keras_model(self):
+        raise NotImplementedError
